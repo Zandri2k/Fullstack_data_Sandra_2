1f3443850d0df642421c4a74c2b8b2dcc3cb3738
Zandri2k <sandrasiphokaziii@gmail.com>
2024-09-18 13:56:03 +0200

    Exercise3_switch_bot_WORKS

--- Exercises/Streamlit_chatbot_dashboard/dashboard.py
+++ Exercises/Streamlit_chatbot_dashboard/dashboard.py
@@ -1,12 +1,12 @@
 import streamlit as st
-from components.bot import Bot
+from components.bot import Bot, GbGlenn_bot_action, JokeTeller_bot_action
 
 def initialize_session_state():
     """Initialize session state variables """
     if "messages" not in st.session_state:
         st.session_state.messages = []
     if "bot" not in st.session_state:
-        st.session_state.bot = Bot()
+        st.session_state.bot = Bot("""You always tell a funny joke, most of the times they're really bad but you finds it funny""")
 
 def display_chat_message():
     """Display chat message from history"""
@@ -23,7 +23,7 @@ def handler_user_input():
         st.session_state.messages.append({"role": "user", "content": prompt})
 
         bot_response = st.session_state.bot.chat(prompt)
-        response = f"GbGlenn: {bot_response}"
+        response = f"Bot: {bot_response}"
 
         with st.chat_message("assistant"):
             st.markdown(response)
@@ -31,10 +31,19 @@ def handler_user_input():
 
 def layout():
     """Define the layout of the Streamlit app"""  
-    st.title("Chat with GbGlenn")
-    st.write("GbGlenn is an extra ordinary chat bot with snapy respones and always close to funny joke.")
+    chosen_bot = st.selectbox("choose one chatbot", options= ["GbGlenn", "JokeTeller"])
+
+    st.title(f"{chosen_bot} was choosen")
+
+    #st.write(f"{chosen_bot} is an extra ordinary chat bot with snapy respones and always close to funny joke.")
     display_chat_message()
     handler_user_input()
+
+    bot_personalities = {"GbGlenn": GbGlenn_bot_action, "JokeTeller": JokeTeller_bot_action }
+    st.session_state.bot = Bot(bot_personalities[chosen_bot])
+
+    bot_introductions = {"GbGlenn": "is an extra ordinary chat bot with snapy respones and always close to funny joke.", "JokeTeller": "JokeTeller will always crack a funny joke"}
+    st.write(f"{bot_introductions[chosen_bot]}")
 
 if __name__ == "__main__":
     initialize_session_state() #This is the memory, state av sessionen.
