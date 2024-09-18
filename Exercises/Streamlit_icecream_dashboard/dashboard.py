5c1907947b844c4ffb042a840a0cf111ea6e0c37
Zandri2k <sandrasiphokaziii@gmail.com>
2024-09-18 09:08:36 +0200

    Exercise2_Icecream_import_working

--- Exercises/Streamlit_icecream_dashboard/dashboard.py
+++ Exercises/Streamlit_icecream_dashboard/dashboard.py
@@ -3,11 +3,15 @@ import pandas as pd
 from pathlib import Path
 
 def read_data():
-    data_path = (__file__).parent[2] / "Data"
+    data_path = Path(__file__).parents[2] / "Data"
     df = pd.read_csv(data_path / "IceCreamData.csv", index_col = 0, parse_dates=[0])
-    df.index = df.index.revenue
+    
     return df
 
 def layout():
     df = read_data()
-    st.markdown ("# ")
+    st.markdown ("# Ice Cream Revenue Prediction")
+    st.dataframe(df)
+
+if __name__ == '__main__':
+    layout()
