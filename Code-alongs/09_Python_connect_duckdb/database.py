de2c4427b2ed79394f5c217b0c5b27f2e681523b
Zandri2k <sandrasiphokaziii@gmail.com>
2024-09-23 13:36:45 +0200

    connecting duck db part1

---
+++ Code-alongs/09_Python_connect_duckdb/database.py
@@ -0,0 +1,29 @@
+import duckdb
+from pathlib import Path
+
+db_path = Path(__file__).parent/"test.db"
+
+class Database:
+    def __init__(self, db_path) -> None:
+        self.db_path = db_path
+        self.connection = None
+    
+    def __enter__(self):
+        self.connection = duckdb.connect(self.db_path)
+        return self
+    
+    def query(self, query):
+        self.connection.execute(query).fetchall()
+
+        
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.connection:
+            self.connection.close()
+
+if __name__ == '__main__':
+    with Database(db_path) as db:
+        query1 = db.query(" SELECT * FROM information_schema.schemata;")
+    
+    print(query1)
+
