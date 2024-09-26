bd155f094a37fcbf824845d241839d295e841605
Zandri2k <sandrasiphokaziii@gmail.com>
2024-09-26 13:42:56 +0200

    Labb_e)_EDA

---
+++ Labb1/Scripts/EDA.sql
@@ -0,0 +1,83 @@
+SELECT  * FROM  datum.totalt;
+
+
+WITH 
+	date_table AS (SELECT * FROM datum.tabelldata OFFSET 1),
+	date_total AS (SELECT * FROM datum.totalt OFFSET 1)
+SELECT 
+	STRFTIME('%Y-%m-%d', tot.datum), 
+	tot.visningar,
+	tab.visningar,
+	tab."visningstid (timmar)"
+FROM date_total as tot
+LEFT JOIN date_table as tab 
+ON tot.datum = tab.datum;
+
+
+SELECT Enhetstyp, count(*) total_rows, sum(Visningar) as total_visningar 
+from 
+enhetstyp.diagramdata group by Enhetstyp ;
+
+select * from enhetstyp.diagramdata d ;
+
+SELECT * EXCLUDE (Innehåll) FROM  innehall.tabelldata ORDER BY "Visningstid (timmar)" DESC OFFSET 1 LIMIT 5;
+
+SELECT * FROM  innehall.diagramdata;-- ORDER BY "Visningstid (timmar)";
+
+SELECT STRFTIME('%Y-%m-%d', Datum), Visningar FROM innehall.totalt;
+
+-- visar antalet visningar, visningstis och genomsnittlig längd
+SELECT * FROM  datum.tabelldata;
+
+SELECT  * FROM geografi.tabelldata;
+
+SELECT * FROM  geografi.diagramdata; -- kollar över geografidatan
+
+-- Show the sum of the views for each country, SE, IN, MT
+SELECT
+
+	Geografi,
+	SUM(visningar)
+FROM
+	geografi.diagramdata
+GROUP BY
+	Geografi
+ORDER BY
+	SUM(visningar);
+
+
+
+SELECT * FROM  enhetstyp.diagramdata; --shows Unit and the amout of views + duration
+
+-- shows the amount of views from each unit
+SELECT
+	enhetstyp,
+	SUM (visningar)
+FROM
+	enhetstyp.diagramdata
+GROUP BY
+	Enhetstyp
+ORDER BY
+	SUM (Visningar)
+	
+--
+SELECT
+	Enhetstyp, 
+	SUM(visningar)
+FROM
+	enhetstyp.diagramdata 
+WHERE 
+	Enhetstyp = 'Surfplatta'
+
+GROUP BY 
+	Enhetstyp
+
+ORDER BY 
+	SUM(visningar);
+
+
+
+
+
+
+
